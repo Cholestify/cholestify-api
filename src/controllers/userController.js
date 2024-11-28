f657530b0c8239d4db3a9c6a6237102926708283
martbakCode <rajendranohan4@gmail.com>
2024-11-28 21:41:01 +0700

    Route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,9 +7,15 @@ class UserController {
     const { name, email, password } = request.payload;
     try {
       const user = await UserService.createUser (name, email, password);
-      return h.response(user).code(201);
+      return h.response({
+        "error": false,
+        "message": "Register successfully",
+        "data": user}).code(201);
     } catch (error) {
-      return h.response({ error: 'User  creation failed' }).code(400);
+      return h.response({
+        "error": true,
+        "message": "Register failed",
+      }).code(400);
     }
   }
 
@@ -18,21 +24,39 @@ class UserController {
     const user = await UserService.findUserByEmail(email);
     if (user && await bcrypt.compare(password, user.password)) {
       const token = jwtUtils.generateToken(user.id, user.email);
-      return h.response({ token }).code(200);
+      return h.response({
+        "error": false,
+        "message": "Login successfully",
+        "data": {
+          "token": token
+        }
+      }).code(200);
     } else {
-        return h.response({ error: 'Invalid email or password' }).code(403);
+        return h.response({
+          "error": true,
+          "message": "Invalid email or password" 
+        }).code(403);
     }
   }
 
   static async getUsers(request, h) {
     const users = await UserService.getAllUsers();
-    return h.response(users).code(200);
+    return h.response({
+      "error": false,
+      "message": "Profile fetched successfully",
+      "data": users
+    }).code(200);
   }
 
   static async deleteUser (request, h) {
     const { id } = request.params;
     await UserService.deleteUser (id);
-    return h.response().code(204);
+    return h.response(
+      {
+        "error": false,
+        "message": "User deleted successfully"
+      }
+    ).code(204);
   }
 }
 
