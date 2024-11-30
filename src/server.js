98d42ab92fab9be5a0b86bed81b00eb4a7cac4a0
fadhil9 <fadhilmuhamad272@gmail.com>
2024-11-30 11:26:04 +0700

    all routes

--- src/server.js
+++ src/server.js
@@ -1,22 +1,25 @@
-const Hapi = require('@hapi/hapi');
-const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes');
+const Hapi = require("@hapi/hapi");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const cholestrolHistoryRoutes = require("./routes/cholestrolHistoryRoutes");
 
 dotenv.config();
 
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
-    host: 'localhost',
+    host: "localhost",
   });
 
-  server.route(userRoutes);
+  const allRoutes = [...userRoutes, ...cholestrolHistoryRoutes];
+
+  server.route(allRoutes);
 
   await server.start();
   console.log(`Server running on ${server.info.uri}`);
 };
 
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
