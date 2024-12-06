7992c2daae2c1961558cdb05536aa11d52f6a4d3
martbakCode <rajendranohan4@gmail.com>
2024-12-06 13:08:37 +0700

    Daily Nutrition

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,13 +1,13 @@
-const { PrismaClient } = require("@prisma/client");
-const bcrypt = require("bcryptjs");
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
 
 const prisma = new PrismaClient();
 
 class UserService {
-  static async createUser(name, email, password) {
+  static async createUser (name, email, password) {
     const hashedPassword = await bcrypt.hash(password, 10);
     return await prisma.user.create({
-      data: { name: name, password: hashedPassword, email: email },
+      data: { name : name,  password: hashedPassword, email: email },
     });
   }
 
@@ -23,46 +23,10 @@ class UserService {
     return await prisma.user.findMany();
   }
 
-  // static async deleteUser(id) {
-  //   return await prisma.user.delete({ where: { id: Number(id) } });
-  // }
-  static async deleteUser(id) {
-    try {
-      return await prisma.user.delete({
-        where: { id: Number(id) },
-      });
-    } catch (error) {
-      throw new Error(`Error deleting user: ${error.message}`);
-    }
+  static async deleteUser (id) {
+    return await prisma.user.delete({ where: { id: Number(id) } });
   }
 
-<<<<<<< HEAD
-  static async updateUser(id, name, email, birthdate, gender, weight, height) {
-    // return await prisma.user.update({
-    //   where: { id: Number(id) },
-    //   data: {
-    //     name: name,
-    //     email: email,
-    //     birthdate: birthdate,
-    //     gender: gender,
-    //     weight: weight,
-    //     height: height
-    //   },
-    // });
-    console.log(id, name, email, birthdate, gender, weight, height);
-    try {
-      const updatedUser = await prisma.user.update({
-        where: { id: Number(id) },
-        data: {
-          name: name,
-          email: email,
-          birthdate: birthdate ? new Date(birthdate) : null,
-          gender: gender,
-          weight: weight || null,
-          height: height || null,
-        },
-      });
-=======
   static async updateUser (id, name, email, birthdate, gender, weight, height, activity) {
     return await prisma.user.update({
       where: { id: Number(id) },
@@ -77,14 +41,7 @@ class UserService {
       },
     });
   }
->>>>>>> d1f31c9 (Daily Nutrition)
 
-      return updatedUser;
-    } catch (error) {
-      console.error("Error updating user:", error.message);
-      throw error;
-    }
-  }
 }
 
 module.exports = UserService;
